fccd17f64ae7746b20c8e1837fe035e3804c67f1
AL-NFLY <almernfly@gmail.com>
2023-10-07 20:56:22 +0700

    feat: Add Box component

---
+++ src/components/Box.tsx
@@ -0,0 +1,13 @@
+interface BoxProps {
+    children: React.ReactNode;
+    classname?: string;
+}
+const Box: React.FC<BoxProps> = ({children, classname}) => {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default Box
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { IoMdHome, IoMdSearch } from "react-icons/io";
+import Box from "./Box";
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -25,7 +26,9 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
     return (
         <div className="flex h-full">
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
-                Navigation
+                <Box>
+                    Navigation
+                </Box>
             </div>
         </div>
     )
