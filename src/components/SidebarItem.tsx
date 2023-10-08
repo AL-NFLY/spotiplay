bea968c6eb9f8687220bac4618330788016a5b13
AL-NFLY <almernfly@gmail.com>
2023-10-08 23:49:59 +0700

    feat: Style layout and transition for SidebarItems

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
 
-const SidebarItem = () => {
+interface SidebarItemProps {
+    icon: IconType;
+    label: string;
+    href: string;
+    active?: boolean;
+}
+const SidebarItem: React.FC<SidebarItemProps> = ({icon, href, label, active}) => {
   return (
-    <div>SidebarItem</div>
+    <Link 
+        href={href}
+        className={twMerge(`flex flex-row h-auto items-center w-full gap-x-4 font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`,
+            active && "text-white"
+        )}
+    >
+        Test 
+    </Link>
   )
 }
 
