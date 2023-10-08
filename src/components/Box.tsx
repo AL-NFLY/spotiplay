a5a8a9109dac0bf46680fca6f537d5f0579d3c02
AL-NFLY <almernfly@gmail.com>
2023-10-08 23:41:29 +0700

    feat: Add SidebarItem component

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,10 +1,15 @@
+import { twMerge } from "tailwind-merge";
+
 interface BoxProps {
     children: React.ReactNode;
     classname?: string;
 }
 const Box: React.FC<BoxProps> = ({children, classname}) => {
   return (
-    <div>
+    <div className={twMerge(`bg-neutral-900 rounded-md w-full h-fit`,
+        classname
+      )}
+    >
         {children}
     </div>
   )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { IoMdHome, IoMdSearch } from "react-icons/io";
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -27,7 +28,17 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
         <div className="flex h-full">
             <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
                 <Box>
-                    Navigation
+                    <div className="flex flex-col gap-y-4 px-5 py-4">   
+                        {routes.map((item) => (
+                            <SidebarItem
+                                key={item.label}
+                                {...item}
+                            />
+                        ))}
+                    </div>
+                </Box>
+                <Box classname="overflow-y-auto h-full">
+                    Library
                 </Box>
             </div>
         </div>
