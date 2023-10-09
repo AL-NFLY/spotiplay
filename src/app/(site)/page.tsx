17c6dd6550fe3533e82450dd33105a11cddf9b88
AL-NFLY <almernfly@gmail.com>
2023-10-09 23:31:20 +0700

    feat: Add Header component

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,14 +1,16 @@
+import Header from '@/components/Header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
+    <main className='bg-neutral-900 h-full w-full rounded-md overflow-hidden overflow-y-auto'>
       {/* <h1 className=''>
         Welcome Back to <span className='text-green-500'>Spotiplay</span>
       </h1> */}
-      <h2 className='text-green-500'>
-        Main Content
-      </h2>
+      <Header>
+      <h2 className='text-green-500'>Test</h2>
+      </Header>
+
     </main>
   )
 }
