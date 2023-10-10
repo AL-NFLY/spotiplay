494881357d2cd99d59de5a3accd8719b629b757f
AL-NFLY <almernfly@gmail.com>
2023-10-10 08:59:44 +0700

    feat: Add Home & Search buttons for mobile view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter } from "next/navigation"
-import { IoChevronBack, IoChevronForward } from "react-icons/io5"
+import { IoChevronBack, IoChevronForward, IoHome, IoSearch } from "react-icons/io5"
 import { twMerge } from "tailwind-merge"
 
 
@@ -22,20 +22,28 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
         )}>
             <div className="w-full mb-4 flex items-center justify-between">
                 <div className="hidden md:flex gap-x-2 items-center">
-                    <button className="rounded-full bg-neutral-900 flex text-center justify-center hover:opacity-80 transition p-1">
-                        <IoChevronBack 
-                            size={25}
-                            className="text-white" 
-                        />
+                    <button 
+                        onClick={() => router.back()} 
+                        className="rounded-full bg-neutral-900 flex text-center justify-center hover:opacity-80 transition p-1">
+                        <IoChevronBack size={25} className="text-white" 
+                    />
                     </button>
-                    <button className="rounded-full bg-neutral-900 flex text-center justify-center hover:opacity-80 transition p-1">
-                        <IoChevronForward 
-                            size={25}
-                            className="text-white" 
-                        />
+                    <button 
+                        onClick={() => router.forward}
+                        className="rounded-full bg-neutral-900 flex text-center justify-center hover:opacity-80 transition p-1">
+                        <IoChevronForward size={25} className="text-white" 
+                    />
                     </button>
                 </div>
 
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button className="flex items-center justify-center rounded-full p-2 bg-neutral-900 hover:opacity-80  transition">
+                        <IoHome size={20} className="text-white"/>
+                    </button>
+                    <button className="flex items-center justify-center rounded-full p-2 bg-neutral-900 hover:opacity-80  transition">
+                        <IoSearch size={20} className="text-white"/>
+                    </button>
+             </div>
             </div>
             Header
         </div>
