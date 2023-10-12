582b1b260debaa8842c5ddf98096bbe54eb7384d
AL-NFLY <almernfly@gmail.com>
2023-10-12 20:20:51 +0700

    refactor: Delete provider folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import SupabaseProvider from '@/Providers/SupabaseProvider'
+import SupabaseProvider from '@/providers/SupabaseProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
