2aa94b7b0ff7d24dfea3d57a18c9c9ccc7a855ce
AL-NFLY <almernfly@gmail.com>
2023-10-17 14:31:20 +0700

    feat: Add UploadModal for upload popup

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,18 +1,21 @@
 'use client'
 
 import useAuthModal from "@/hooks/useAuthModal"
+import useUploadModal from "@/hooks/useUploadModal "
 import { useUser } from "@/hooks/useUser"
 import Image from "next/image"
 import { IoAdd, IoAlbums } from "react-icons/io5"
 
 const Library = () => {
     const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
     const { user } = useUser();
     const onClick = () => {
         if(!user) {
             return authModal.onOpen();
         }
-
+        // TODO: Check for subscription
+        return uploadModal.onOpen();
     }
     return (
         <div className="flex flex-col">
