2a88cf6be9b7593c6eed853b7424ce0718202cdc
AL-NFLY <almernfly@gmail.com>
2023-10-25 15:52:30 +0700

    feat: Add /search app

--- src/actions/getSongsByTitle.ts
+++ src/actions/getSongsByTitle.ts
@@ -3,31 +3,26 @@ import { Song } from "../../types"
 import { cookies } from "next/headers"
 import getSongs from "./getSongs"
 
-const getSongsByUserId = async (): Promise<Song[]> => {
+const getSongsByTitle = async (title: string): Promise<Song[]> => {
   const supabase = createServerComponentClient({
     cookies: cookies
   })
 
-  const {
-    data: sessionData,
-    error: sessionError,
-  } = await supabase.auth.getSession()
-
-  if (sessionError) {
-    console.log(sessionError.message)
-    return []
+  if (!title) {
+    const allSongs = await getSongs();
+    return allSongs
   }
-  
+
   const { data, error } = await supabase
     .from('songs')
     .select('*')
-    .eq('user_id', sessionData.session?.user.id)
+    .ilike('title', `%${title}%`)
     .order('created_at', { ascending: false })
 
-  if (error) {
-    console.log(error.message)
-  }
-  return (data as any) || [];
+    if (error) {
+      console.log(error)
+    }
+    return (data as any) || []
 }
 
-export default getSongsByUserId;
+export default getSongsByTitle;
