b82a18f05a964a89d640e5b9be169843697b3b16
AL-NFLY <almernfly@gmail.com>
2023-10-26 17:07:55 +0700

    feat: Add logic if there is no liked songs

--- src/app/liked/components/LikedContent.tsx
+++ src/app/liked/components/LikedContent.tsx
@@ -21,6 +21,14 @@ const LikedContent: React.FC<LikedContentProps> = ({songs}) => {
     }
   }, [isLoading, user, router]);
 
+  if (songs.length === 0) {
+    return (
+        <div className="flex flex-col gap-y-2 px-6 h-1/2 items-center justify-center">
+            <p className="text-2xl font-semibold">No liked songs found</p>
+            <p className="text-sm">Feel free to start liking your favorite songs.</p>
+        </div>
+    )
+  }
   
   return (
     <div>LikedContent</div>
