e0e1c83b94909443d6cd793f08650cdc5756d3b7
AL-NFLY <almernfly@gmail.com>
2023-10-30 12:37:43 +0700

    feat: Add Player.tsx component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
 import getSongsByUserId from '@/actions/getSongsByUserId'
+import Player from '@/components/Player'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -31,9 +32,12 @@ export default async function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
+
             <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
+
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
