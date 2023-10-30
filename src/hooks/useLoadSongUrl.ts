42f134f3277fc9e1456cd819a14a14e83901c7f5
AL-NFLY <almernfly@gmail.com>
2023-10-30 12:52:13 +0700

    feat: Create useLoadSongUrl.ts

---
+++ src/hooks/useLoadSongUrl.ts
@@ -0,0 +1,22 @@
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
+import { Song } from "../../types";
+
+const useLoadSongUrl = (song: Song) => {
+
+//   non-auth users can play songs; change to useSessionContext() for auth users only
+  const supabaseClient = useSupabaseClient();
+
+
+  if (!song) {
+    return '';
+  }
+
+  const { data: songData } = supabaseClient
+    .storage
+    .from('songs')
+    .getPublicUrl(song.song_path);
+
+  return songData.publicUrl;
+};
+
+export default useLoadSongUrl
