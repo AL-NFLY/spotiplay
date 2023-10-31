c0970edbbd6b2339e6344931baff807216ede71a
AL-NFLY <almernfly@gmail.com>
2023-10-31 11:15:02 +0700

    feat: Add volume button in PlayerContent.tsx

--- src/components/PlayerContent.tsx
+++ src/components/PlayerContent.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import { BsPlay, BsPlayFill } from "react-icons/bs";
+import { BsPlay, BsVolumeDown, BsVolumeMute, BsVolumeMuteFill } from "react-icons/bs";
 import { Song } from "../../types"
 import LikedButton from "./LikedButton";
 import MediaItem from "./MediaItem";
 import { IoIosSkipBackward, IoIosSkipForward, IoMdPause, IoMdSkipBackward, IoMdSkipForward } from "react-icons/io";
-import { IoBackspace } from "react-icons/io5";
+import { HiOutlineVolumeOff, HiOutlineVolumeUp } from "react-icons/hi";
 
 interface PlayerContentProps {
     song: Song;
@@ -14,7 +14,8 @@ interface PlayerContentProps {
 
 const PlayerContent: React.FC<PlayerContentProps> = ({song, songUrl}) => {
   const Icon = true ? IoMdPause : BsPlay
-  
+  const VolumeIcon = true ? HiOutlineVolumeOff : HiOutlineVolumeUp
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
         <div className="flex w-full justify-start">
@@ -28,35 +29,44 @@ const PlayerContent: React.FC<PlayerContentProps> = ({song, songUrl}) => {
             </div>
         </div>
 
-            {/* Mobile View for Pause/Play Button */}
-            <div className="flex md:hidden col-auto w-full justify-end items-center">
-                <div
-                    onClick={() => {}}
-                    className="flex h-9 w-9 items-center justify-center rounded-full p-1 cursor-pointer bg-white"
-                >
-                    <Icon className="text-black w-5 h-auto"/>
-                </div>
+        {/* Mobile View for Pause/Play Button */}
+        <div className="flex md:hidden col-auto w-full justify-end items-center">
+            <div
+                onClick={() => {}}
+                className="flex h-9 w-9 items-center justify-center rounded-full p-1 cursor-pointer bg-white"
+            >
+                <Icon className="text-black w-5 h-auto"/>
+            </div>
+        </div>
+
+        {/* Desktop View */}
+        <div className="hidden w-full h-full max-w-screen-md gap-x-6 md:flex justify-center items-center">
+            <IoIosSkipBackward 
+                onClick={() => {}}
+                className="w-6 h-auto text-neutral-400 hover:text-white cursor-pointer transition"
+            />
+
+            <div
+                onClick={() => {}} 
+                className="flex items-center justify-center rounded-full h-9 w-9 bg-white"
+            >
+                <Icon className="text-black w-5 h-auto"/>
             </div>
 
-            {/* Desktop View */}
-            <div className="hidden w-full h-full max-w-screen-md gap-x-6 md:flex justify-center items-center">
-                <IoIosSkipBackward 
-                    onClick={() => {}}
-                    className="w-6 h-auto text-neutral-400 hover:text-white cursor-pointer transition"
-                />
+            <IoIosSkipForward 
+                onClick={() => {}}
+                className="w-6 h-auto text-neutral-400 hover:text-white cursor-pointer items-center transition"
+            />
+        </div>
 
-                <div
-                    onClick={() => {}} 
-                    className="flex items-center justify-center rounded-full h-9 w-9 bg-white"
-                >
-                    <Icon className="text-black w-5 h-auto"/>
-                </div>
-
-                <IoIosSkipForward 
+        <div className="hidden md:flex w-full justify-end">
+            <div className="flex items-center w-32 gap-x-2">
+                <VolumeIcon 
                     onClick={() => {}}
-                    className="w-6 h-auto text-neutral-400 hover:text-white cursor-pointer items-center transition"
-                />
+                    className="w-5 h-auto text-neutral-400 hover:text-white` cursor-pointer"/>
             </div>
+        </div>
+
     </div>
   )
 }
