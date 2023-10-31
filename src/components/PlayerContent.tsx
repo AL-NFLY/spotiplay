cdd80a6c3843c4fcbc5d91773ef15db6556d1edc
AL-NFLY <almernfly@gmail.com>
2023-10-31 10:41:39 +0700

    feat: Add play, pause, and skip buttons for desktop view

--- src/components/PlayerContent.tsx
+++ src/components/PlayerContent.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import { BsPlay, BsPlayFill } from "react-icons/bs";
 import { Song } from "../../types"
 import LikedButton from "./LikedButton";
 import MediaItem from "./MediaItem";
+import { IoIosSkipBackward, IoIosSkipForward, IoMdPause, IoMdSkipBackward, IoMdSkipForward } from "react-icons/io";
+import { IoBackspace } from "react-icons/io5";
 
 interface PlayerContentProps {
     song: Song;
@@ -10,8 +13,10 @@ interface PlayerContentProps {
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({song, songUrl}) => {
+  const Icon = true ? IoMdPause : BsPlay
+  
   return (
-    <div className="grid md:grid-cols-3 h-full">
+    <div className="grid grid-cols-2 md:grid-cols-3 h-full">
         <div className="flex w-full justify-start">
             <div className="flex gap-x-4">
                 <MediaItem 
@@ -19,6 +24,25 @@ const PlayerContent: React.FC<PlayerContentProps> = ({song, songUrl}) => {
                 />
                 <LikedButton
                     songId={song.id}
+                />
+            </div>
+
+            {/* Desktop View */}
+            <div className="hidden w-full h-full max-w-screen-md gap-x-6 md:flex justify-center items-center">
+                <IoIosSkipBackward 
+                    onClick={() => {}}
+                    className="w-6 h-auto text-neutral-400 hover:text-white cursor-pointer transition"
+                />
+
+                <div
+                    onClick={() => {}} 
+                    className="flex items-center justify-center rounded-full h-9 w-9 bg-white">
+                    <Icon className="text-black w-5 h-auto"/>
+                </div>
+
+                <IoIosSkipForward 
+                    onClick={() => {}}
+                    className="w-6 h-auto text-neutral-400 hover:text-white cursor-pointer items-center"
                 />
             </div>
         </div>
