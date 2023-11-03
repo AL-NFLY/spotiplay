7f2733514de42d53b305b3e58d3bb863f30069f2
AL-NFLY <almernfly@gmail.com>
2023-11-03 21:30:52 +0700

    feat: Change text layout in /about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,8 +11,8 @@ const page = () => {
     <div className="w-full h-full rounded-md bg-neutral-900 overflow-hidden overflow-y-auto">
         <Header>
             <>
-                <div className="flex flex-col md:flex-row items-center gap-x-5">
-                    <div className="relative h-48 w-48 lg:h-52 lg:w-52">
+                <div className="flex flex-col md:flex-row gap-x-5 md:items-center">
+                    <div className="relative h-48 w-48 lg:h-52 lg:w-52 max-sm:m-auto">
                         <Image
                             src='/images/profile.jpg'
                             alt='Almer'
@@ -20,9 +20,9 @@ const page = () => {
                             className="object-cover"
                         />
                     </div>
-                    <div className="flex flex-col md:gap-y-2 mt-4 md:mt-0">
+                    <div className="flex flex-col md:gap-y-2 mt-4 md:mt-0 ">
                         <p>Hi there!</p>
-                        <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold">I&apos;m Almer</h2>
+                        <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold">I&apos;m Almer Naufaly</h2>
                     </div>
                 </div>
 
