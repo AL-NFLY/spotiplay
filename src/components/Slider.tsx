358ec5abde76fd7ed25a7fa70e52b1a1eced3c58
AL-NFLY <almernfly@gmail.com>
2023-11-04 10:49:46 +0700

    feat: Add animation for slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -22,8 +22,8 @@ const Slider: React.FC<SliderProps> = ({value = 1, onChange}) => {
         aria-label="Volume"
         className="relative flex items-center w-full h-10 select-none touch-none"
     >
-        <RadixSlider.Track className="relative grow rounded-full h-1 bg-neutral-600">
-            <RadixSlider.Range className="absolute rounded-full h-full bg-white" />
+        <RadixSlider.Track className="group relative grow rounded-full h-1 bg-neutral-600">
+            <RadixSlider.Range className="absolute rounded-full h-full bg-white group-hover:bg-green-500" />
         </RadixSlider.Track>
     </RadixSlider.Root>
   )
