0d97a74b1e67069b15b13e5cec0cc1efeda23648
AL-NFLY <almernfly@gmail.com>
2023-11-05 07:43:56 +0700

    refactor: Change semantics of Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
             h-fit bg-gradient-to-b from-emerald-800 p-6
             `, className
         )}>
-            <div className="w-full mb-4 flex items-center justify-between">
+            <nav className="w-full mb-4 flex items-center justify-between">
                 <div className="hidden md:flex gap-x-2 items-center">
                     <button 
                         onClick={() => router.back()} 
@@ -100,7 +100,7 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
                         </>
                     )}
                 </div>
-            </div>
+            </nav>
             {children}
         </section>
     )
